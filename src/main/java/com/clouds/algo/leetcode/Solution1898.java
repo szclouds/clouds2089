5e4fe4232fe320a48b9b961a3f186166c33529ea
clouds <lpspzsy@gmail.com>
2022-09-04 00:56:45 +0800

    feat():1898-可移除字符的最大数目-实现

--- src/main/java/com/clouds/algo/leetcode/Solution1898.java
+++ src/main/java/com/clouds/algo/leetcode/Solution1898.java
@@ -1,5 +1,8 @@
 package com.clouds.algo.leetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author clouds
  * @version 1.0
@@ -8,13 +11,56 @@ public class Solution1898 {
     /**
      * 可移除字符的最大数目
      *
-     * @param s 字符串
-     * @param p s 的一个 子序列
+     * @param s         字符串
+     * @param p         s 的一个 子序列
      * @param removable 从 0 开始 计数的整数数组 removable
      * @return 结果
      * @link <a href="https://leetcode.cn/problems/maximum-number-of-removable-characters/">...</a>
      */
     public int maximumRemovals(String s, String p, int[] removable) {
-          return 0;
+        int low = 0;
+        int high = removable.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            String str = getStr(s, removable, mid);
+            if (checksSubsequence(str, p)) {
+                if (mid + 1 == removable.length || !checksSubsequence(getStr(s, removable, mid + 1), p)) {
+                    return mid + 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                high = mid - 1;
+            }
+        }
+        return 0;
+    }
+
+    private String getStr(String s, int[] nums, int index) {
+        StringBuilder sb = new StringBuilder();
+        Set<Integer> set = new HashSet<>();
+        index = Math.min(index, nums.length - 1);
+        for (int i = 0; i <= index; i++) {
+            set.add(nums[i]);
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (!set.contains(i)) {
+                String str = String.valueOf(s.charAt(i));
+                sb.append(str);
+            }
+        }
+        return sb.toString();
+    }
+
+    private boolean checksSubsequence(String t, String s) {
+        int n = s.length(), m = t.length();
+        int i = 0, j = 0;
+        while (i < n && j < m) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == n;
     }
 }
--- src/test/java/com/clouds/algo/leetcode/Solution1898Test.java
+++ src/test/java/com/clouds/algo/leetcode/Solution1898Test.java
@@ -12,8 +12,9 @@ class Solution1898Test {
     @Test
     void maximumRemovals() {
         Solution1898 solution1898 = new Solution1898();
-        assertEquals(solution1898.maximumRemovals("abcacb", "ab", new int[]{3, 1, 0}), 2);
-        assertEquals(solution1898.maximumRemovals("abcbddddd", "abcd", new int[]{3, 2, 1, 4, 5, 6}), 1);
-        assertEquals(solution1898.maximumRemovals("abcab", "abc", new int[]{0, 1, 2, 3, 4}), 0);
+        assertEquals(2, solution1898.maximumRemovals("abcacb", "ab", new int[]{3, 1, 0}));
+        assertEquals(1, solution1898.maximumRemovals("abcbddddd", "abcd", new int[]{3, 2, 1, 4, 5, 6}));
+        assertEquals(0, solution1898.maximumRemovals("abcab", "abc", new int[]{0, 1, 2, 3, 4}));
+        assertEquals(2, solution1898.maximumRemovals("qlevcvgzfpryiqlwy", "qlecfqlw", new int[]{12,5}));
     }
 }
