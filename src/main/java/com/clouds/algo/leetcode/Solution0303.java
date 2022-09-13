a495ba1b2405f70104a77e1410bffdd6f13de414
clouds <lpspzsy@gmail.com>
2022-09-13 23:57:45 +0800

    init():0303-区域检索

---
+++ src/main/java/com/clouds/algo/leetcode/Solution0303.java
@@ -0,0 +1,25 @@
+package com.clouds.algo.leetcode;
+
+/**
+ * @author clouds
+ * @version 1.0
+ */
+public class Solution0303 {
+    private int[] nums;
+
+    public Solution0303(int[] nums) {
+        this.nums = nums;
+    }
+
+    /**
+     * 区域和检索 - 数组不可变
+     *
+     * @param left  左索引
+     * @param right 右索引
+     * @return 计算索引 left 和 right （包含 left 和 right）之间的 nums 元素的 和 ，其中 left <= right
+     * @link <a href="https://leetcode.cn/problems/range-sum-query-immutable/">...</a>
+     */
+    public int sumRange(int left, int right) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/clouds/algo/leetcode/Solution0303Test.java
@@ -0,0 +1,15 @@
+package com.clouds.algo.leetcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author clouds
+ * @version 1.0
+ */
+class Solution0303Test {
+
+    @Test
+    void sumRange() {
+    }
+}
