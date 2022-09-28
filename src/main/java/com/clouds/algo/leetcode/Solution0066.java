b5a276152b5960cff194a8e01c69b2f2328bec95
clouds <lpspzsy@gmail.com>
2022-09-28 23:58:32 +0800

    init():0066-加一操作

---
+++ src/main/java/com/clouds/algo/leetcode/Solution0066.java
@@ -0,0 +1,24 @@
+package com.clouds.algo.leetcode;
+
+/**
+ * @author clouds
+ * @version 1.0
+ */
+public class Solution0066 {
+
+    /**
+     * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+     * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+     * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+     * 来源：力扣（LeetCode）
+     * 链接：
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * @param digits 原始数组
+     * @return 加一后的数组
+     * @link <a href="https://leetcode.cn/problems/plus-one">...</a>
+     */
+    public int[] plusOne(int[] digits) {
+        return new int[0];
+    }
+}
---
+++ src/test/java/com/clouds/algo/leetcode/Solution0066Test.java
@@ -0,0 +1,15 @@
+package com.clouds.algo.leetcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author clouds
+ * @version 1.0
+ */
+class Solution0066Test {
+
+    @Test
+    void plusOne() {
+    }
+}
