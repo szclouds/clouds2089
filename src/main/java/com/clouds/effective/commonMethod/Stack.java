c516b15108af34d23a5b8832d71ab767e75f9653
clouds <lpspzsy@gmail.com>
2022-11-15 23:51:50 +0800

    init():stack clone

---
+++ src/main/java/com/clouds/effective/commonMethod/Stack.java
@@ -0,0 +1,49 @@
+package com.clouds.effective.commonMethod;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+/**
+ * @author clouds
+ * @version 1.0
+ */
+public class Stack {
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public Stack() {
+        this.elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(Object e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public Object pop() {
+        if (size == 0)
+            throw new EmptyStackException();
+        Object result = elements[--size];
+        elements[size] = null; // Eliminate obsolete reference
+        return result;
+    }
+
+    // Ensure space for at least one more element.
+    private void ensureCapacity() {
+        if (elements.length == size)
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+    }
+
+    // Clone method for class with references to mutable state
+    @Override
+    public Stack clone() {
+        try {
+            Stack result = (Stack) super.clone();
+            result.elements = elements.clone();
+            return result;
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        }
+    }
+}
